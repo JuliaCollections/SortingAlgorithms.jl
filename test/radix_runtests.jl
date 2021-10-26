74a4e77696cdd35496702a14f682708cbe0e776a
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-10-26 08:49:02 -0500

    style: negligible

--- test/radix_runtests.jl
+++ test/radix_runtests.jl
@@ -1,8 +1,9 @@
-include("../test/common.jl")
-
 include("../src/serialize.jl")
 include("../src/radix.jl")
 include("../src/radix_wrapper.jl")
+
+include("../test/common.jl")
+
 include("../test/radix_wrapper.jl")
 
 include("../test/lohi_patch.jl")
