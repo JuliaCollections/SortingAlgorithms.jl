033385b7029fea7b7e916c98616c02364a68896a
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-11-20 17:47:50 -0600

    extend overall benchmarking slightly

--- radix_benchmarks.jl
+++ radix_benchmarks.jl
@@ -1,7 +1,17 @@
 using SortingAlgorithms, SortMark
 
-df = SortMark.make_df([QuickSort, MergeSort, RadixSort2])
+df = SortMark.make_df([RadixSort2, QuickSort, MergeSort])
 expected = (df.Type .∉ [[UInt64, UInt128]]) .| (df.order .!= [Base.Order.Reverse]) .| (df.source_key .!= [:small_positive])
 df = df[expected, :]
-compute!(df, fail_fast=false)
-stat!(df)
+compute!(df)
+stat!(df, 1, 2)
+
+x1 = df[(df.source_key .== :simple) .& (df.len .> 100) .& (df.Type .== Int) .&
+    ((df.order .== [Base.Order.Forward]) .| (df.len .> 400)), :]
+x1.seconds .= .05
+compute!(x1)
+stat!(x1, 1, 2)
+println("always an improvement: ", maximum(first.(x1.confint)) < 1)
+#Yay!
+
+#Much still to do...
