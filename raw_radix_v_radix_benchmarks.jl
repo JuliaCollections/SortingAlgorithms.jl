83cbcda195f6272f1c11d4fe6d897f69e6831bc3
Lilith Orion Hafner <60898866+LilithHafner@users.noreply.github.com>
2021-11-19 20:57:04 -0600

    extend benchmarks to all UInts

--- raw_radix_v_radix_benchmarks.jl
+++ raw_radix_v_radix_benchmarks.jl
@@ -11,17 +11,19 @@ function Base.sort!(v::AbstractVector{<:Unsigned}, lo::Integer, hi::Integer, ::R
     v
 end
 
-df = SortMark.make_df([RawRadixSort2, RadixSort],
-    Types=[UInt128], orders=[Base.Order.Forward],
-    lens=SortMark.lengths(2, 10_000_000, 3),
-    sources=Dict(:simple=>SortMark.sources[:simple]),
-    seconds=nothing, samples=30)
-compute!(df)
-stat!(df)
-display(df[:, [:len, :Type, :pvalue, :point_estimate, :confint]])
+for T in sort(SortMark.UInts, by=sizeof)
+    df = SortMark.make_df([RawRadixSort2, RadixSort],
+        Types=[T], orders=[Base.Order.Forward],
+        lens=SortMark.lengths(2, 2_000_000, 5),
+        sources=Dict(:simple=>SortMark.sources[:simple]),
+        seconds=nothing, samples=20)
+    compute!(df)
+    stat!(df)
+    display(df[:, [:len, :Type, :pvalue, :point_estimate, :confint]])
 
-plot(log10.(df.len), first.(df.confint),
-    title="95% confidence interval or runtime ratio: This/That",
-    xlabel="log10(length)",
-    legend=false)
-plot!(log10.(df.len), last.(df.confint))
+    plot(log10.(df.len), first.(df.confint),
+        title="$T: 95% CI of runtime ratio: This/That",
+        xlabel="log10(length)",
+        legend=false)
+    display(plot!(log10.(df.len), last.(df.confint)))
+end
