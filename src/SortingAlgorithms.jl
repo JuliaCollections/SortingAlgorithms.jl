af75499307dfa24a4b2d839c66bfc5d383cded72
Nicolau Leal Werneck <nwerneck@gmail.com>
2022-08-27 12:54:27 +0200

    juliacon reference

--- src/SortingAlgorithms.jl
+++ src/SortingAlgorithms.jl
@@ -39,6 +39,7 @@ Characteristics:
 
 ## References
  - H. Inoue, T. Moriyama, H. Komatsu and T. Nakatani, "AA-Sort: A New Parallel Sorting Algorithm for Multi-Core SIMD Processors," 16th International Conference on Parallel Architecture and Compilation Techniques (PACT 2007), 2007, pp. 189-198, doi: 10.1109/PACT.2007.4336211.
+ - Werneck, N. L., (2020). ChipSort: a SIMD and cache-aware sorting module. JuliaCon Proceedings, 1(1), 12, https://doi.org/10.21105/jcon.00012
 """
 const CombSort  = CombSortAlg()
 
